d99580278fec33a4e97ea6718b3b65713ad93a52
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-28 14:48:14 +0300

    adding pilow for images

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+  image = models.ImageField()
+  image_name = models.CharField(max_length=40)
+  image_description = models.TextField()
+  image_location = models.ForeignKey('Location', on_delete = models.CASCADE)
+  category = models.ForeignKey('Category', on_delete = models.CASCADE)
+
+  # def __str__(self):
+  #     return self.image
+ 
+  # def save_image(self):
+  #   pass
+
+  # def delete_image(self):
+  #   pass
+
+  # def update_image(self, image):
+  #   pass
+
+  # def get_image_by_id(self, id):
+  #   pass
+
+  @classmethod
+  def search_image(category):
+    pass
+
+  @classmethod
+  def filter_by_location(location):
+    pass
+
+
+class Location(models.Model):
+  location = models.ManyToManyField(Image)
+  def __str__(self):
+      return self.location
+
+class Category(models.Model):
+  category = models.CharField(max_length=40)
+
+  def __str__(self):
+      return self.category
