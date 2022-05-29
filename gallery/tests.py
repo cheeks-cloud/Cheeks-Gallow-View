dd11f600b13c7ca99e9a8b1d855444d0e089631f
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-29 10:06:15 +0300

    writing testing methods

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
-# Create your tests here.
+
+class ImageTestClass(TestCase):
+  def setup(self):
+    pass
+  def test_instance_(self):
+    pass
+  def test_save_image(self):
+    pass
+
+  def test_delete_image(self):
+    pass
+
+  def test_update_image(self):
+    pass
+
+  def test_get_image_by_id(self):
+    pass
+
+  def test_search_image(self,category):
+    pass
+
+  def test_filter_by_location(self,location):
+    pass
+
