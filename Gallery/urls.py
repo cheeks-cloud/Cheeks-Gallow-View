30ad5c904bd6f6cde7e7a06b51d0fdd0216a0700
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-29 09:07:56 +0300

    adding methods to the class image

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gallery/',include('gallery.urls'))
+    path('',include('gallery.urls'))
 ]
--- gallery/models.py
+++ gallery/models.py
@@ -2,38 +2,45 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-  image = models.ImageField()
-  image_name = models.CharField(max_length=40)
-  image_description = models.TextField()
+  image = models.ImageField(null=False, blank=False, upload_to='images/')
+  image_name = models.CharField(max_length=40,null=False, blank=False)
+  image_description = models.TextField(null=False, blank=False)
   image_location = models.ForeignKey('Location', on_delete = models.CASCADE)
-  category = models.ForeignKey('Category', on_delete = models.CASCADE)
+  image_ category = models.ForeignKey('Category', on_delete = models.CASCADE)
 
   def __str__(self):
       return self.image
  
   def save_image(self):
-    pass
+    self.save()
 
+  @classmethod
   def delete_image(self):
     pass
 
-  def update_image(self, image):
-    pass
-
+  def update_image(self, new_name, new_description, new_image, new_category, new_location):
+      self.image = new_image
+      self.image_name = new_name
+      self.image_location = new_location
+      self.image_description = new_description
+      self.image_category = new_category
   def get_image_by_id(self, id):
     pass
 
   @classmethod
-  def search_image(category):
-    pass
+  def search_image(cls, category):
+    image_to_find = Image.objects.filter(category=category)
+    return image_to_find
 
   @classmethod
-  def filter_by_location(location):
-    pass
+  def filter_by_location(cls,location):
+    image_by_location = Image.objects.filter(location=location)
+    return image_by_location
 
 
 class Location(models.Model):
-  location = models.ManyToManyField(Image)
+  name = models.ManyToManyField(Image)
+
   def __str__(self):
       return self.location
 
