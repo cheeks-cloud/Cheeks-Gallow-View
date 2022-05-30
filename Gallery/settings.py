4dc518959431382d970ca9bcfaedcdb75c1821c8
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-30 20:36:03 +0300

    fixing deployment bug

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -1,18 +1,39 @@
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
+MODE=config("MODE",default="dev")
+SECRET_KEY = config("SECRET_KEY")
+DEBUG=config("DEBUG",default=False,cast=bool)
+if config('MODE')=="dev":
+    DATABASES = {
+        'default':{
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '', 
+        }
+    }
+else:
+        DATABASES = {
+            'default':dj_database_url.config(
+                default=config('DATABASE_URL')
+            )
+        }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sczaiww+-pkcgic41k^j9&$zdioz8ox&h45&##3dz89(d$b^ip'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -62,17 +83,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Gallery.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME': 'mygallery',
-        'USER':'moringa',
-        'PASSWORD':'cheeks'
-    }
-}
 
 
 # Password validation
@@ -114,6 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -122,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
---
+++ static/css/index.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+//   $('.info').hide()
+
+//   $('.image-card').on('click', function() {
+//       $(this).children('.image').addClass('blur')
+//       $(this).children('.details').fadeIn( "slow")
+//   })
+
+//   $('.image-card').on('mouseleave', function() {
+//       $(this).children('.image').removeClass('blur')
+//       $(this).children('.details').fadeOut( "slow")
+//   })
+// })
+
+// function copy_url(data){
+//   navigator.clipboard.writeText(window.location.origin + data)
+// }
